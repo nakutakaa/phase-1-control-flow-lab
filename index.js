54ccf07b7b1a28b10d6d0751fd3d93514e1e70fd
Mkalabai John <nakutakaa@gmail.com>
2025-03-14 01:32:26 +0300

    control_flows

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(fistStop) {
   // Write your code here!
+  if (fistStop <= 400) {
+    return 'this is on me!'
+  }
+  else if (fistStop > 400 && fistStop <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (fistStop > 2000 && fistStop <= 2500) {
+    return 'I will gladly take your thirty bucks'
+  }
+  else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityCheck){
   // Write your code here!
+  return cityCheck === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charmTip) {
+  switch (charmTip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.'
+  }
+
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,7 @@ require ( './helpers.js' );
 describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+      expect(scuberGreetingForFeet(199)).to.equal('this is on me!');
     });
 
     it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
@@ -11,7 +11,7 @@ describe('index.js', function () {
     });
 
     it('charges 30 dollars for a distance over 2000 feet', function () {
-      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks');
     });
 
     it('does not allow rides over 2500 feet', function () {
